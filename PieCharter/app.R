528a62168d5be30069bc20a32c7a5e23a3fda021
dungates <gatesdu@oregonstate.edu>
2021-01-04 19:06:52 -0800

    uploading to github

---
+++ PieCharter/app.R
@@ -0,0 +1,133 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(tidyverse)
+library(here)
+library(scales) # For graphing scales
+library(shiny)
+library(shinyWidgets)
+library(DT)
+library(grDevices)
+teaching_df <- read_rds(here("Data/final_df.rds")) # Read in the data
+cols <- c("professional_training_session", 
+          "select_your_site_district_parish_or_network", 
+          "select_the_grade_band_s_you_focused_on", 
+          "learning_session_satisfaction",
+          "todays_topic_was_relevant_for_my_role",
+          "designed_to_help_me_learn",
+          "likely_to_apply_46_weeks",
+          "select_the_name_of_your_first_facilitator",
+          "s_he_facilitated_the_content_clearly_12",
+          "did_you_have_a_second_facilitator",
+          "select_the_name_of_your_second_facilitator",
+          "s_he_facilitated_the_content_clearly_16",
+          "s_he_effectively_built_a_community_of_learners_17") 
+col = grDevices::colorRampPalette(c("#040404", "#04ABEB"))
+
+sidebarPanel2 <- function (..., out = NULL, width = 4) 
+{
+    div(class = paste0("col-sm-", width), 
+        tags$form(class = "well", ...),
+        out
+    )
+}
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+  
+  tags$head(tags$style(
+    HTML('
+         #sidebar {
+            background-color: #dec4de;
+        }
+
+        body, label, input, button, select { 
+          font-family: "Arial";
+        }')
+  )),
+  
+    setBackgroundColor(
+      color = c("#F7FBFF", "#2171B5"),
+      gradient = "linear",
+      direction = "bottom"
+    ),
+    
+  
+    # Application title
+    titlePanel("Pie Chart Maker"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel2(
+            out = h5("Quick (very ugly) Shiny App I made to visualize all discrete variables with pie charts, there are obviously better visualization tools
+                     for pie charts, but I wanted to demonstrate Shiny competency. The variable names are still in their 'dirty' form but should
+                     at least be decipherable."),
+            selectInput("data", cols, width = '600px',
+                        label = h3("Select Variable:")), # Basic Shiny Input Take
+            textOutput("textData")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           wellPanel(plotOutput("piePlot", width = "100%")),
+           DTOutput("tableData"),
+           HTML('<style type="text/css">
+        .span8 .well { background-color: #00FFFF; }
+        </style>')
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    mydata <- reactive({
+        teaching_df %>%
+            dplyr::group_by(get(input$data)) %>% # Group by input variable
+            dplyr::summarise(n = n()) %>% # Get count of variable
+            ungroup() %>% # Ungroup
+            dplyr::mutate(percent = round(100 * (n / sum(n)), 2)) # Make a percent column
+    })
+    
+    output$piePlot <- renderPlot({
+        ggplot2::ggplot(mydata(), aes(x = 0, y = n, fill = reorder(`get(input$data)`, n))) + #Pie chart input, ordered by n
+            labs(fill = "Type", x = NULL, y = NULL,
+                 title = paste("Percentage of", as.character(input$data)),
+                 subtitle = "Variables with greater than 5% occurrence") +
+            geom_bar(stat = "identity", color = "gray", width = 0.5) + # Using bar columns put in polar coordinates later
+            geom_text(aes(label = ifelse(percent > 5,
+                                         paste0(percent, "%\n", `get(input$data)`),
+                                         paste("")),
+                          x = 0.4),
+                      position = position_stack(vjust = 0.5), #position stack normally stacks bars, but here it keeps the text in the right place once put in polar
+                      size = 3,
+                      fontface = "bold") + # Add bold text with percentage and variable label
+            scale_x_continuous(expand = c(0, 0)) + # Change x so expand is not default and adds no padding so the bars will produce a circle not a donut
+            coord_polar(theta = "y", direction = -1) + # Make bars polar
+            scale_fill_manual(values = c(col(nrow(mydata())))) + # custom colors
+            theme_void() + # Remove all panel elements
+            theme(legend.position = "none",
+                  plot.title = element_text(hjust = 0.5),
+                  plot.subtitle = element_text(hjust = 0.5),
+                  plot.background = element_rect(fill = "skyblue"))
+    })
+
+    output$tableData <- DT::renderDT({
+        DT::datatable(mydata(), colnames = c(paste(str_to_title(input$data), "Group"), "Number", "Percent"))
+    })
+    
+    output$textData <- renderText({ 
+        paste("You have selected", str_to_title(input$data))
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ README.md
@@ -0,0 +1,20 @@
+# Teaching Lab Performance Task
+
+<p align = "center">
+<b>Response to Option B</b>
+</p>
+
+This repository consists of parts to address each of the 3 "competencies" from part 2. The Directors of Partnerships will probably be most interested in the output of [FriendorColleague.html](FriendorColleague.html) where I visualize the "scores" of every single training session by each curriculum portfolio: EL, Guidebooks, Illustrative Mathematics (IM), or State-level work. Code is contained in the rmd folder where 5 separate R markdowns are used to obtain, clean, visualize, and edit the data. Four of these R Markdowns are knit to html files which are easy to view and what I intend as the "presentations" from part c. The PieCharter rmd can be knit to a shiny document, or in the PieCharter folder a Shiny App can be run that is also available as a shiny web app [here](https://dungates.shinyapps.io/PieCharter/).
+
+In [DataObtainandClean.rmd](https://github.com/dungates/TeachingLab/blob/master/Rmd/DataObtainandClean.Rmd) I go over how I got the data with code that can easily be replicated by cloning from Github. This code will run on other hard drives through the use of the here library and the corresponding Data folder where I saved all my data after completing the cleaning process. [DataObtainandClean.html](DataObtainandClean.html) has the process of how I got the data and cleaned it with code output.
+
+
+<p align = "center">
+<b>In every html output of the r markdowns you can click images to view them in greater detail.</b>
+</p>
+
+Next in the other 3 R Markdown documents  I create separate analyses with technical details and visualizations to address part c, I could put them in a some slide-making tool but I feel that being able to scroll through them is most useful. I am addressing the ability
+
+The 4 html files that result from knitting each of these is copied into the main folder for ease of access, and each is outlined below.
+
+[LearningSessionSatisfaction.html](LearningSessionSatisfaction.html) looks at the column on learning session satisfaction, seeing how each instructor has fared over time, and also looking at all of them on a numeric basis, addressing the new column requirement. [SentimentAnalysis.html](SentimentAnalysis.html) has a complete textual analysis that aggregates text by column and across columns when there is textual response, and looks at several different methods/models of analyzing the data. [FriendorColleague.html](FriendorColleague.html) is almost exclusively visual and plots every single professional learning session separately by the only numeric column in the data - likeliness to recommend to a friend or colleague. Finally [PieCharter.rmd](https://dungates.shinyapps.io/PieCharter) introduces shiny functionality to create pie charts of factor columns which can facilitate such visualizations. Prettier pie charts can definitely be made with other tools but I thought the shiny functionality within an r markdown was pretty cool as I just discovered it.
